a8dc9c3a7fb381fcd7fee6f52389366d3b48e1a3
Jakub Nesveda <jakubnesveda@seznam.cz>
2011-07-29 10:24:17 +0200

    continue

--- src/application/Bootstrap.php
+++ src/application/Bootstrap.php
@@ -7,6 +7,14 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->bootstrap('view');
         $view = $this->getResource('view');
         $view->doctype('XHTML1_STRICT');
+        
+        $loader = new Zend_Loader_Autoloader_Resource (array (
+          'basePath' => APPLICATION_PATH,
+          'namespace' => 'Application',
+        ));
+        
+        $loader->addResourceType ( 'model', 'models', 'Model'); 
+        $loader->addResourceType ( 'form', 'forms', 'Form');
     }
 }
 
